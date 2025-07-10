ddb1d15348f0a6db764497dcac547eeb1386138c
gabrielcs77 <109834806+gabrielcs77@users.noreply.github.com>
2025-07-10 09:15:10 -0300

    Add files via upload

---
+++ app/matriz/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import MatrizGE from '../components/MatrizGE';
+import AuthenticatedLayout from '../components/AuthenticatedLayout';
+
+export default function MatrizGEPage() {
+  return (
+    <AuthenticatedLayout>
+      <MatrizGE />
+    </AuthenticatedLayout>
+  );
+} 
